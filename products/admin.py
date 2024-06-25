b30280367995a8550f9400db214ac847dd074607
Mehar Zain <zainzainpubg1122@gmail.com>
2024-06-25 00:57:39 -0700

    12:57-6-25-24

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import products
 # Register your models here.
 class productsAdmin(admin.ModelAdmin):
-    list_display = ['title','description','price','category']
+    list_display = ['title','description','price','category','rating','total_amount_products']
     
 admin.site.register(products,productsAdmin)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from static import *
 # Create your models here.
 class products(models.Model):
     title = models.CharField(max_length=60)
     description = models.TextField(max_length=120)
     price = models.IntegerField()
-    category = models.TextField(max_length=60)
+    category = models.CharField(max_length=60)
+    total_amount_products = models.IntegerField()
+    rating = models.IntegerField()
